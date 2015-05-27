53ab08c11da46f7a4c99eb1f3757ab352680910b
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2015-05-27 12:25:27 +0200

    Reorganize controllers and services on client.

---
+++ client/controllers/products.js
@@ -0,0 +1,5 @@
+(function() {
+  'use strict';
+  var productModule = angular.module("ProductModule",['ngResource','ui.router']);
+
+}());
---
+++ client/services/users.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  var userModule = angular.module('UserModule');
+
+  userModule.factory('usersService',['$resource',function($resource){
+    var usersActions = {
+      'get':{method:'GET'},
+      'save': {method: 'POST'},
+      'query': {method: 'GET', isArray: true},
+      'update': {method: 'PUT'},
+      'delete': {method: 'DELETE'}
+    },
+    db = {};
+    db.users = $resource('/api/users/:_id',{},usersActions);
+    return db;
+  }]);
+
+  userModule.factory('loginService',['$resource',function($resource){
+    var loginActions = {
+      'post':{method:'post'}
+    },
+    db = {};
+    db.login = $resource('/api/login',{},loginActions);
+    return db;
+  }]);
+}());
