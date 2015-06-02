c756e7d9bf09c284331a84b1ad1c766298e051f8
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2015-06-02 12:48:16 +0200

    Just add some in-code doc.

--- client/controllers/products.js
+++ client/controllers/products.js
@@ -1,5 +1,11 @@
 (function() {
   'use strict';
+
+  /**
+  * ProductModule : Angular module to deal with product views
+  *
+  * Dependencies : ngResource , ui.router
+  */
   var productModule = angular.module("ProductModule",['ngResource','ui.router']);
 
 }());
--- client/controllers/users.js
+++ client/controllers/users.js
@@ -4,8 +4,17 @@
 
 (function() {
   'use strict';
+
+  /**
+  * UserModule : Angular module to deal with user views
+  *
+  * Dependencies : ngResource , ui.router
+  */
   var userModule = angular.module("UserModule",['ngResource','ui.router']);
 
+  /**
+  * LoginSignupCtrl : 
+  */
   userModule.controller("LoginSignupCtrl",function($scope,$location,loginService){
     $scope.loginClick = function(user){
       loginService.login.post(
