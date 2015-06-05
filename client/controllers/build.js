59d5694da7c1683a636af5eb9739ccb9103484e2
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2015-06-05 12:47:55 +0200

    Sorry guys. forgot to add the build controller script in index.html/

--- client/controllers/build.js
+++ client/controllers/build.js
@@ -4,21 +4,24 @@
 (function() {
   'use strict';
 
-  var buildModule = angular.module('BuildModule',['ngResource','ui.router','ngCookies']);
+  var buildModule = angular.module('BuildModule',['ngResource','ui.router']);
 
   buildModule.controller('MainBuildCtrl',function($scope,$state){
-
+    console.log('Main Build Controller');
   });
 
   buildModule.controller('LeftMenuBuildCtrl',function($scope,$state){
+    console.log('Left Menu Build Controller');
 
   });
 
   buildModule.controller('RightMenuBuildCtrl',function($scope,$state){
+    console.log('Right Menu Build Controller');
 
   });
 
   buildModule.controller('BottomMenuBuildCtrl',function($scope,$state){
+    console.log('Bottom MenuBuild Controller');
 
   });
 }());
