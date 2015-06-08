778ffeaba381cd82d4c57de4b2228a3668bdb3b2
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2015-06-08 16:48:15 +0200

    Build bottom menu assited bey the controller.

--- client/controllers/build.js
+++ client/controllers/build.js
@@ -23,9 +23,29 @@
   buildModule.controller('BottomMenuBuildCtrl',function($scope,$state,productService){
     console.log('Bottom MenuBuild Controller');
 
+    $scope.CATEGORIES = {
+      SKELETON : 'skeleton',
+      SCREEN : 'screen',
+      PROCESSOR : 'processor',
+      BATTERY : 'battery',
+      STORAGE : 'storage',
+      CONNECTION : 'connection',
+      CAMERA : 'camera',
+      SPEAKER : 'speaker'
+    };
 
 
+    $scope.currentCategory = $scope.CATEGORIES.SKELETON;
     $scope.productsFromCurrentCategory = [];
+    $scope.categories = [{name : $scope.CATEGORIES.SKELETON, iconImgPath : '../assets/images/icons/SkeletonColor.png'},
+                         {name : $scope.CATEGORIES.SCREEN, iconImgPath : '../assets/images/icons/ScreenColor.png'},
+                         {name : $scope.CATEGORIES.PROCESSOR, iconImgPath : '../assets/images/icons/ProcessorColor.png'},
+                         {name : $scope.CATEGORIES.BATTERY, iconImgPath : '../assets/images/icons/BatteryColor.png'},
+                         {name : $scope.CATEGORIES.STORAGE, iconImgPath : '../assets/images/icons/StorageColor.png'},
+                         {name : $scope.CATEGORIES.CONNECTION, iconImgPath : '../assets/images/icons/ConnectionColor.png'},
+                         {name : $scope.CATEGORIES.CAMERA, iconImgPath : '../assets/images/icons/CameraColor.png'},
+                         {name : $scope.CATEGORIES.SPEAKER, iconImgPath : '../assets/images/icons/SpeakerColor.png'},
+                         ];
 
     $scope.loadCategory = function(category){
       productService.productsByCategory.get({category:category},function(resObj){
