46377513488c357d59ecdbfe65cc376dbd29a8e1
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2015-06-09 04:08:28 +0200

    Add functions to deal with the go back during the design of the phone.

--- client/controllers/build.js
+++ client/controllers/build.js
@@ -4,9 +4,9 @@
 (function() {
   'use strict';
 
-  var buildModule = angular.module('BuildModule',['ngResource','ui.router','ngCookies','ProductModule']);
+  var buildModule = angular.module('BuildModule',['ngResource','ui.router','ngCookies','ProductModule','CommonModule']);
 
-  buildModule.controller('MainBuildCtrl',function($scope,$state,$rootScope){
+  buildModule.controller('MainBuildCtrl',function($scope,$state,$rootScope,$cookies,$interval,sessionService){
     $rootScope.phone = {
       skeleton : {},
       front : [],
@@ -17,8 +17,72 @@
       $rootScope.productOnFocus = null;
     };
 
-    $rootScope.historic = [$rootScope.phone];
-    $rootScope.hitoricPointer = 0;
+    $rootScope.history = [$rootScope.phone];
+    $rootScope.historyPointer = 0;
+
+    $rootScope.CATEGORIES = {
+      SKELETON : 'skeleton',
+      SCREEN : 'screen',
+      PROCESSOR : 'processor',
+      BATTERY : 'battery',
+      STORAGE : 'storage',
+      CONNECTION : 'connection',
+      CAMERA : 'camera',
+      SPEAKER : 'speaker'
+    };
+
+    $rootScope.FRONT_OR_BACK = {
+      FRONT : 'front',
+      BACK : 'back',
+    };
+
+    $scope.removeOutdatedHistory = function(){
+      for (var i = $rootScope.history.lenght - 1 - $rootScope.historyPointer ; i > array.length; i--) {
+        $rootScope.history.pop();
+      }
+    };
+
+    $rootScope.saveState = function(){
+      $rootScope.history[$rootScope.historyPointer] = $rootScope.phone;
+      $rootScope.historyPointer += 1;
+    };
+
+    $rootScope.addProductToCart = function(product,customization,position){
+      var isSkeleton = false,
+          item = {};
+      product.categories.forEach(function(category){
+        isSkeleton = category === $rootScope.CATEGORIES.SKELETON ? true : false;
+      });
+
+      item.product = product;
+      item.customization = customization;
+
+      if (!isSkeleton) {
+        item.position.frontOrBack = position.frontOrBack;
+        item.position.tilePosition = position.tilePosition;
+
+        if (position.frontOrBack === $rootScope.FRONT_OR_BACK.FRONT) {
+          $rootScope.phone.front.push(item);
+        }
+
+        if (position.frontOrBack === $rootScope.FRONT_OR_BACK.BACK) {
+          $rootScope.phone.back.push(item);
+        }
+      }else{
+        $rootScope.phone.skeleton = item;
+      }
+
+      $scope.removeOutdatedHistory();
+      $rootScope.saveState();
+    };
+
+    $scope.saveCart = function(){
+
+    };
+
+    $scope.$on('$viewContentLoaded',function(){
+      
+    });
   });
 
   buildModule.controller('LeftMenuBuildCtrl',function($scope,$state){
@@ -56,49 +120,39 @@
     * STOP EREASING.
     */
 
-    $scope.CATEGORIES = {
-      SKELETON : 'skeleton',
-      SCREEN : 'screen',
-      PROCESSOR : 'processor',
-      BATTERY : 'battery',
-      STORAGE : 'storage',
-      CONNECTION : 'connection',
-      CAMERA : 'camera',
-      SPEAKER : 'speaker'
-    };
-
-    $scope.currentCategory = $scope.CATEGORIES.SKELETON;
+    $scope.currentCategory = $rootScope.CATEGORIES.SKELETON;
     $scope.productsFromCurrentCategory = [];
-    $scope.categories = [{name : $scope.CATEGORIES.SKELETON, iconImgPath : '../assets/images/icons/SkeletonColor.png'},
-                         {name : $scope.CATEGORIES.SCREEN, iconImgPath : '../assets/images/icons/ScreenColor.png'},
-                         {name : $scope.CATEGORIES.PROCESSOR, iconImgPath : '../assets/images/icons/ProcessorColor.png'},
-                         {name : $scope.CATEGORIES.BATTERY, iconImgPath : '../assets/images/icons/BatteryColor.png'},
-                         {name : $scope.CATEGORIES.STORAGE, iconImgPath : '../assets/images/icons/StorageColor.png'},
-                         {name : $scope.CATEGORIES.CONNECTION, iconImgPath : '../assets/images/icons/ConnectionColor.png'},
-                         {name : $scope.CATEGORIES.CAMERA, iconImgPath : '../assets/images/icons/CameraColor.png'},
-                         {name : $scope.CATEGORIES.SPEAKER, iconImgPath : '../assets/images/icons/SpeakerColor.png'},
+    $scope.categories = [{name : $rootScope.CATEGORIES.SKELETON,
+                          iconImgPath : '../assets/images/icons/SkeletonColor.png'},
+                         {name : $rootScope.CATEGORIES.SCREEN,
+                          iconImgPath : '../assets/images/icons/ScreenColor.png'},
+                         {name : $rootScope.CATEGORIES.PROCESSOR,
+                          iconImgPath : '../assets/images/icons/ProcessorColor.png'},
+                         {name : $rootScope.CATEGORIES.BATTERY,
+                          iconImgPath : '../assets/images/icons/BatteryColor.png'},
+                         {name : $rootScope.CATEGORIES.STORAGE,
+                          iconImgPath : '../assets/images/icons/StorageColor.png'},
+                         {name : $rootScope.CATEGORIES.CONNECTION,
+                          iconImgPath : '../assets/images/icons/ConnectionColor.png'},
+                         {name : $rootScope.CATEGORIES.CAMERA,
+                          iconImgPath : '../assets/images/icons/CameraColor.png'},
+                         {name : $rootScope.CATEGORIES.SPEAKER,
+                          iconImgPath : '../assets/images/icons/SpeakerColor.png'},
                          ];
 
     $scope.loadCategory = function(category){
       productService.productsByCategory.get({category:category},function(resObj){
-        $scope.productsFromCurrentCategory = resObj.products;
-        $rootScope.refresh();
-      });
-    };
-
-    $scope.focusProduct = function(product){
-      $rootScope.productOnFocus = product;
-    };
-
-    $scope.$on('$viewContentLoaded',function(){
-      productService.productsByCategory.get({category:'camera'},function(resObj){
         if (resObj.err) {
           $scope.productsFromCurrentCategoryError = resObj.err;
         }else{
           $scope.productsFromCurrentCategory = resObj.products;
         }
       });
-    });
+    };
+
+    $scope.focusProduct = function(product){
+      $rootScope.productOnFocus = product;
+    };
 
   });
 }());
